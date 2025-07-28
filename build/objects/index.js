7be7e1d27f9e54dd51e0a9d5995b518f3a10b769
Federico <federico.grasso606@gmail.com>
2025-07-28 17:27:37 +0200

    Minor changes

--- build/objects/index.js
+++
@@ -1,6 +0,0 @@
-import { Coin } from "./coin.js";
-import { Snake } from "./snake.js";
-import { Obstacle } from "./obstacle.js";
-import { BonusPlayer } from './bonusplayer.js';
-import { MalusPlayer } from './malusplayer.js';
-export { Coin, Snake, Obstacle, BonusPlayer, MalusPlayer };
--- build/types/index.js
+++
@@ -1,4 +0,0 @@
-import { ScreenEdge, Direction, Speed } from './enums.js';
-import { ClockTick, ClockType, Timer } from './timer.js';
-import { Position } from './position.js';
-export { ScreenEdge, Direction, Speed, Position, ClockTick, ClockType, Timer };
